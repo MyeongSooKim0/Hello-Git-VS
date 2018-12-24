520889d45474f1a723a5b5089ac4be11c8395a21
김명수 <smk7893@protnc.co.kr>
2018-12-24 14:10:55 +0900

    test!!!

--- Project5/Project5/소스.cpp
+++ Project5/Project5/소스.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(void) {
 	char str[] = "Hello Git";
 	cout << str << endl;
-	int i = 0;
-	for (i = 1; i < 5; i++) {
+	
+	for (int i = 1; i < 5; i++) {
 		cout << i << "¹ø Â°" << str << " " << endl;
 	}
 	return 0;
